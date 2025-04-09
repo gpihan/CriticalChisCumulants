2b1cb6b0b72914d36ddd90c045b9117fadcb432f
Greg <gregoirepihan@euclide-II.local>
2025-04-08 19:32:18 -0500

    Initial setup

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    cout << "That's a first step" << endl;
+    return 0;
+}
