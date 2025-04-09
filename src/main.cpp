138fb6a06eb02bee043c5cd19ee15f222bbaf272
Greg <gregoirepihan@euclide-II.local>
2025-04-08 19:46:00 -0500

    Add surface files reading functions

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,130 @@
 
 using namespace std;
 
+typedef struct surfaceElement {
+    double x[4];            // position in (tau, x, y, eta)
+    double sinh_eta_s;      // caching the sinh and cosh of eta_s
+    double cosh_eta_s;
+    double s[4];            // hypersurface vector in (tau, x, y, eta)
+    double u[4];            // flow velocity in (tau, x, y, eta)
+    double W[4][4];         // W^{\mu\nu}
+    double q[4];            // baryon diffusion current
+    double pi_b;            // bulk pressure
+    double rho_B;           // net baryon density
+
+    double epsilon_f;
+    double T_f;
+    double mu_B;
+    double mu_S;
+    double mu_C;
+    double eps_plus_p_over_T_FO;  // (energy_density+pressure)/temperature
+} SurfaceElement;
+
+int get_number_of_lines_of_binary_surface_file(string filename) {
+    std::ifstream surface_file(filename.c_str(), std::ios::binary);
+    int count = 0;
+    float temp = 0.;
+    while(surface_file) {
+        surface_file.read((char*) &temp, sizeof(float));
+        count++;
+    }
+    int counted_line = count/34;/////THIS NUMBER IS MAYBE WRONG
+    surface_file.close();
+    return(counted_line);
+}
+
+
+vector<SurfaceElement> ReadFreezeOutSurface(string Surfpath) {
+    cout << "reading freeze-out surface" << endl;
+
+    vector<SurfaceElement> surface;
+
+    bool boost_invariant = true;
+    ostringstream surfdat_stream;
+    surfdat_stream << Surfpath;
+
+    // new counting, mac compatible ...
+    int NCells = get_number_of_lines_of_binary_surface_file(surfdat_stream.str());
+
+    cout << "NCells = " << NCells << endl;
+    flush(cout);
+
+    ifstream surfdat;
+    surfdat.open(surfdat_stream.str().c_str(), std::ios::binary);
+
+    // Now allocate memory: array of surfaceElements with length NCells
+    //surface = (SurfaceElement *) malloc((NCells)*sizeof(SurfaceElement));
+    int i = 0;
+    while (i < NCells) {
+        SurfaceElement temp_cell;
+        float array[34];
+        for (int ii = 0; ii < 34; ii++) {
+            float temp = 0.;
+            surfdat.read((char*)&temp, sizeof(float));
+            array[ii] = temp;
+        }
+        temp_cell.x[0] = array[0];
+        temp_cell.x[1] = array[1];
+        temp_cell.x[2] = array[2];
+        temp_cell.x[3] = array[3];
+        if (boost_invariant) {
+            temp_cell.x[3] = 0.0;
+        }
+
+        temp_cell.s[0] = array[4];
+        temp_cell.s[1] = array[5];
+        temp_cell.s[2] = array[6];
+        temp_cell.s[3] = array[7];
+
+        temp_cell.u[0] = array[8];
+        temp_cell.u[1] = array[9];
+        temp_cell.u[2] = array[10];
+        temp_cell.u[3] = array[11];
+
+        temp_cell.epsilon_f            = array[12];
+        temp_cell.T_f                  = array[13];
+        temp_cell.mu_B                 = array[14];
+        temp_cell.mu_S                 = array[15];
+        temp_cell.mu_C                 = array[16];
+        temp_cell.eps_plus_p_over_T_FO = array[17];
+
+        temp_cell.W[0][0] = array[18];
+        temp_cell.W[0][1] = array[19];
+        temp_cell.W[0][2] = array[20];
+        temp_cell.W[0][3] = array[21];
+        temp_cell.W[1][1] = array[22];
+        temp_cell.W[1][2] = array[23];
+        temp_cell.W[1][3] = array[24];
+        temp_cell.W[2][2] = array[25];
+        temp_cell.W[2][3] = array[26];
+        temp_cell.W[3][3] = array[27];
+
+        temp_cell.pi_b  = array[28];
+        temp_cell.rho_B = array[29];
+
+        temp_cell.q[0] = array[30];
+        temp_cell.q[1] = array[31];
+        temp_cell.q[2] = array[32];
+        temp_cell.q[3] = array[33];
+
+        temp_cell.sinh_eta_s = sinh(temp_cell.x[3]);
+        temp_cell.cosh_eta_s = cosh(temp_cell.x[3]);
+
+        if (temp_cell.epsilon_f < 0)  {
+            cout << "epsilon_f < 0.!" << endl;
+            exit(1);
+        }
+        if (temp_cell.T_f < 0) {
+            cout << "T_f < 0.!" << endl;
+            exit(1);
+        }
+        surface.push_back(temp_cell);
+        i++;
+    }
+    surfdat.close();
+    return surface;
+}
 
 int main() {
-    cout << "That's a first step" << endl;
     return 0;
 }
